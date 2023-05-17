8703caf7ed3222c5c03f9e6566331e587e83b557
Dawidowsky7 <fanciak@wp.pl>
2023-05-17 20:54:23 +0200

    Implemented BaseModel

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ y = df["target"]
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=42
 )
+
+
+class BaseModel:
+    """Base class for models"""
+
+    def train(self, data: pd.DataFrame) -> None:
+        """Train the model with data"""
+        raise NotImplementedError
+
+    def predict(self, data: pd.DataFrame) -> Any:
+        """Predict using the trained model with data"""
+        raise NotImplementedError
