b4cc2b3d3ddafa0c5c92f17992286f5b0ce618f6
Kamil <kamilkotkowski17@gmail.com>
2023-05-17 21:42:31 +0200

    Implement HeartDiseasePredictor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier  # type: ignore
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("heart.csv")
@@ -19,3 +20,20 @@ class BaseModel:
     def predict(self, data: pd.DataFrame) -> Any:
         """Predict using the trained model with data"""
         raise NotImplementedError
+
+
+class HeartDiseasePredictor(BaseModel):
+    """Heart Disease Predictor class"""
+
+    def __init__(self) -> None:
+        self.model = RandomForestClassifier()
+
+    def train(self, data: pd.DataFrame) -> None:
+        """Train the HeartDiseasePredictor model with data"""
+        x_train_data = data.drop("target", axis=1)
+        y_train_data = data["target"]
+        self.model.fit(x_train_data, y_train_data)
+
+    def predict(self, data: pd.DataFrame) -> Any:
+        """Predict using the trained HeartDiseasePredictor model with data"""
+        return self.model.predict(data)
